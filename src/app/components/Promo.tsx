86cfb04d5624e1908c9b71dd59b7eb7b91030c7f
Ivana Koceva <ivana.koceva10@gmail.com>
2024-03-12 11:04:08 +0100

    fixes

--- src/app/components/Promo.tsx
+++ src/app/components/Promo.tsx
@@ -8,7 +8,7 @@ export default function Promo() {
         <section className="my-10 md:mx-12 mx-auto px-8">
             <div className="rounded bg-eestec md:px-12 py-6 px-8">
                 <h3 className="lg:text-6xl text-5xl text-white font-eestec font-semibold mt-6 mb-2">Want more <span>workshops</span> like this?</h3>
-                <p className="text-white font-eestec font-light mb-8">Join EESTEC LC Skopje and be the first to know about our dynamic events.</p>
+                <p className="text-white font-eestec font-light mb-8">Join EESTEC LC Skopje and be the first to know about our upcoming events.</p>
                 <SecondaryButton text={"Get Involved Now!"} icon={MdElectricBolt}></SecondaryButton>
 
             </div>
